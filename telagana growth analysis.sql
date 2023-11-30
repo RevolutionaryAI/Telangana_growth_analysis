80de0c40bbe974d6bb8b6f08a9cea233c9fe313e
TANISH BALSARA <100031546+RevolutionaryAI@users.noreply.github.com>
2023-11-30 07:48:23 +0530

    Update telagana growth analysis.sql

--- telagana growth analysis.sql
+++ telagana growth analysis.sql
@@ -4,22 +4,22 @@ select * from fact_ts_ipass;
 # List down the top 5 districts that showed the highest document registration revenue growth between FY 2019 and 2022. 
  select * from fact_stamps;
 SELECT c.district,sum(b.documents_registered_rev) as total_doc_registered FROM dim_date a
-JOIN fact_stamps b ON a.month = b.month
-JOIN dim_districts c ON b.dist_code = c.dist_code
+join fact_stamps b ON a.month = b.month
+join dim_districts c ON b.dist_code = c.dist_code
 WHERE fiscal_year between 2019 and 2022
 GROUP BY c.district
-ORDER BY total_doc_registered desc limit 5;
+order BY total_doc_registered desc limit 5;
 
 #2. How does the revenue generated from document registration compare to the revenue generated from e-stamp challans across districts? 
 #List down the top 5 districts where e-stamps revenue contributes significantly more to the revenue than the documents in FY 2022? 
-SELECT c.district,SUM(b.estamps_challans_rev) AS total_estamps,SUM(b.documents_registered_rev) AS total_doc_registered FROM dim_date a
-JOIN fact_stamps b ON a.month = b.month
-JOIN dim_districts c ON b.dist_code = c.dist_code
+select c.district,SUM(b.estamps_challans_rev) AS total_estamps,SUM(b.documents_registered_rev) AS total_doc_registered from dim_date a
+join fact_stamps b ON a.month = b.month
+join dim_districts c ON b.dist_code = c.dist_code
 WHERE fiscal_year = 2022
 GROUP BY c.district
 HAVING SUM(b.estamps_challans_rev) > SUM(b.documents_registered_rev)
 ORDER BY total_doc_registered DESC
-LIMIT 5;
+limit 5;
 
 #3. What would be counts of the e-Stamp challan and document registration for each fiscal year?
 SELECT a.fiscal_year,SUM(b.estamps_challans_cnt) AS total_estamps_count,SUM(b.documents_registered_cnt) AS total_doc_registered_count FROM dim_date a
